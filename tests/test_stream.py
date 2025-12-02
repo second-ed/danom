54c66f712be0e66e74e2c4dc6ca7d0df2db6a268
ed cuss <edcussmusic@gmail.com>
2025-12-02 19:02:25 +0000

    feat: compose multiple filters and maps

--- tests/test_stream.py
+++ tests/test_stream.py
@@ -15,3 +15,12 @@ def test_stream_pipeline(it, expected_part1, expected_part2):
 
     assert part1.map(lambda x: x + 2).filter(lambda x: x % 3 == 0).collect() == expected_part1
     assert part2.collect() == expected_part2
+
+
+def test_stream_with_multiple_fns():
+    assert (
+        Stream.from_iterable(range(10))
+        .map(lambda x: x * 2, lambda x: x + 1)
+        .filter(lambda x: x % 5 == 0, lambda x: x < 10)
+        .collect()
+    ) == (5,)
